6a010b890159caf69dc29145ff8591d2784681c2
Hasan Al Banna <rizbanulhasan08@gmail.com>
2020-03-18 15:47:57 +0600

    added all files

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/dhrubo.css">
+    <link rel="stylesheet" href="css/naeeb.css">
+    <link rel="stylesheet" href="css/omi.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montaga" rel="stylesheet">
+    <script type="text/javascript" src="js/dhrubo.js"></script>
+    <script type="text/javascript" src="js/naeeb.js"></script>
+    <script type="text/javascript" src="js/omi.js"></script>
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
+    <script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+  crossorigin="anonymous"></script>
+   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min.js"></script>
+
+   </script>
+    </script>
+
+    </script>
+
+    </script>
+    <title>Omnispace</title>
+  </head>
+  <body>
+    <?php
+
+        include 'index-parts/part-1.php';
+
+        include 'index-parts/part-2.php';
+
+        include 'index-parts/part-3.php';
+
+        include 'index-parts/part-4.php';
+
+        include 'index-parts/part-5.php';
+
+        include 'index-parts/part-6.php';
+
+        include 'index-parts/part-7.php';
+
+        include 'index-parts/part-8.php';
+
+        include 'index-parts/part-9.php';
+
+
+?>
+  </body>
+</html>
+
+<?php  ?>
