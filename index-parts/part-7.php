82f8cb62d57cebfe991bf8eb973ae75152db21e4
Hasan Al Banna <rizbanulhasan08@gmail.com>
2020-03-24 19:09:25 +0600

    added slider

---
+++ index-parts/part-7.php
@@ -0,0 +1,48 @@
+<!-- <div class="container-fluid">
+  <div class="row d_clients">
+    <div class="col-6 col-sm-4 col-md-3">
+      <img class="d_sing_img" src="img/brac.png" alt="brac">
+    </div>
+    <div class="col-6 col-sm-4 col-md-3">
+      <img class="d_sing_img" src="img/bitopi_logo.png" alt="brac">
+    </div>
+    <div class="col-6 col-sm-4 col-md-3">
+      <img class="d_sing_img" src="img/sunbeamslogo.png" alt="brac">
+    </div>
+  </div>
+</div> -->
+<!-- partial:index.partial.html -->
+<div class="owl-slider">
+<div id="carousel" class="owl-carousel">
+	<div class="item">
+		<img class="owl-lazy" data-src="https://i.pinimg.com/originals/84/67/26/846726299dc5abbeb5d60016f0fb32e9.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="https://i.pinimg.com/originals/84/67/26/846726299dc5abbeb5d60016f0fb32e9.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="">
+	</div>
+  	<div class="item">
+		<img class="owl-lazy" data-src="https://i.pinimg.com/originals/84/67/26/846726299dc5abbeb5d60016f0fb32e9.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="https://i.pinimg.com/originals/84/67/26/846726299dc5abbeb5d60016f0fb32e9.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="">
+	</div>
+  	<div class="item">
+		<img class="owl-lazy" data-src="https://i.pinimg.com/originals/84/67/26/846726299dc5abbeb5d60016f0fb32e9.jpg" alt="">
+	</div>
+	<div class="item">
+		<img class="owl-lazy" data-src="http://desktop-backgrounds-org.s3.amazonaws.com/400x300/twitter-nature-high-definition.jpg" alt="">
+	</div>
+</div>
+</div>
