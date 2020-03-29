670879f831639c1c3098fbb30b781d3e9fdb41f6
Naeeb MD Ridwan <naeeb@Naeebs-MacBook-Pro.local>
2020-03-29 18:38:59 +0600

    service containers

--- index-parts/part-4.php
+++ index-parts/part-4.php
@@ -1,1 +1,237 @@
 <h1>Naeeb is here</h1>
+<div class="container-fluid serviceContainer">
+
+            <h2 class="wow slideInDown" id="serviceTag">SERVICES</h2>
+
+            <div class="row">
+
+                <div class="col-md-4 col-xs-4 serviceColumn1">
+                    <!--<button type="button" class="btn btn-primary serviceButton" data-container="body" data-toggle="popover" data-placement="top" title="Content Management" data-content="From full length feature stories to website content, ghost writing blogs or editing annual reports."><span class="glyphicon glyphicon glyphicon-pencil"></span><br> Content<br> Management</button>-->
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal">
+                        <span class="glyphicon glyphicon glyphicon-pencil"></span>
+                        <br> Content
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel"><strong>Content</strong></h4>
+                                </div>
+                                <div class="modal-body">
+                                    <!--<p class="list">From full length feature stories to website content, ghost writing blogs or editing annual reports, translating articles to transcribing interviews, Omnispace will deliver what you need.
+We offer all services in both Bengali and English.</p><br>-->
+
+                                    <li class="list">Editing</li>
+                                    <li class="list">Translation</li>
+                                    <li class="list">Transcription</li>
+                                    <li class="list">Online Content</li>
+                                    <li class="list">Newspaper Article/Report</li>
+                                    <li class="list">Case Study</li>
+                                    <li class="list">Press Release</li>
+                                    <li class="list">Blog</li>
+                                    <li class="list">Annual Report</li>
+                                    <li class="list">Script</li>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>
+                    <br>
+                    <button type="button" class="wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal2">
+                        <span class="glyphicon glyphicon-search"></span>
+                        <br> Digital
+                        <br> Marketing
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel2"><strong>Digital <br> Marketing </strong></h4>
+                                </div>
+                                <div class="modal-body">
+                                    <!--<p class="list">From full length feature stories to website content, ghost writing blogs or editing annual reports, translating articles to transcribing interviews, Omnispace will deliver what you need.
+We offer all services in both Bengali and English.</p><br>-->
+
+                                    <li class="list">Search Engine Optimisation (SEO)</li>
+                                    <li class="list">Analytics Consulting</li>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-md-4 col-xs-4 serviceColumn2">
+
+                    <button type="button" class=" wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal3">
+                        <span class="glyphicon glyphicon glyphicon-thumbs-up"></span>
+                        <br>Social
+                        <br>Media
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel3">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="cross close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel3"><strong>Social Media</strong></h4>
+                                </div>
+                                <div class="modal-body">
+
+                                    <li class="list">Social Media Consultancy</li>
+                                    <li class="list">Social Profile/Content Creation</li>
+                                    <li class="list">Social Media Monitoring</li>
+                                    <li class="list">Social Media Advertising</li>
+                                    <li class="list">Social Media Analytics</li>
+
+                                    <br>
+                                    <!-- <p>You tell us what you want and what your products are and we develop a specific social media strategy for the appropriate platforms. Our team will develop content and run competitions for your community. With your inputs and information, we will engage with the community, advise and answer questions round the clock.
+We will monitor and measure our progress and report for further review and refinement. And this cycle goes on.
+</p>-->
+                                    <br>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+
+                    <button type="button" class=" wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal4">
+                        <span class="glyphicon glyphicon-picture"></span>
+                        <br>Creative
+                        <br> Design
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel4">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="cross close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel4"><strong>Creative Design</strong></h4>
+                                </div>
+                                <div class="modal-body">
+
+                                    <li class="list">Logo</li>
+                                    <li class="list">Banner</li>
+                                    <li class="list">Social Media Post</li>
+                                    <li class="list">Poster</li>
+                                    <li class="list">Layout</li>
+                                    <li class="list">Catalogue</li>
+                                    <li class="list">Magazine </li>
+                                    <li class="list">Newspaper</li>
+                                    <li class="list">Company Brochure</li>
+                                    <br>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 col-xs-4 serviceColumn3">
+
+                    <button type="button" class=" wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal5">
+                        <span class="glyphicon glyphicon-home"></span>
+                        <br>Web &
+                        <br>App
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel5">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="cross close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel5"><strong>Web & App</strong></h4>
+                                </div>
+                                <div class="modal-body">
+
+                                    <li class="list">Website Design & Development</li>
+                                    <li class="list">Mobile App Development</li>
+                                    <li class="list">Website Modification</li>
+
+                                    <br>
+                                    <!-- <p>You tell us what you want and what your products are and we develop a specific social media strategy for the appropriate platforms. Our team will develop content and run competitions for your community. With your inputs and information, we will engage with the community, advise and answer questions round the clock.
+We will monitor and measure our progress and report for further review and refinement. And this cycle goes on.
+</p>-->
+                                    <br>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <br>
+
+                    <button type="button" class=" wow flipInX btn btn-primary serviceButton" data-toggle="modal" data-target="#myModal6">
+                        <span class="glyphicon glyphicon-book"></span>
+                        <br>Printing
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal6" tabindex="-1" role="dialog" aria-labelledby="myModalLabel6">
+                        <div class="modal-dialog modalBackground" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="cross close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel6"><strong>Printing</strong></h4>
+                                </div>
+                                <div class="modal-body">
+
+                                    <li class="list">Banner</li>
+                                    <li class="list">Poster</li>
+                                    <li class="list">Catalogue</li>
+                                    <li class="list">Magazine</li>
+                                    <li class="list">Brocheure</li>
+                                    <li class="list">Reports</li>
+
+                                    <br>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- serviceContainer Ends here-->
+        </div>
