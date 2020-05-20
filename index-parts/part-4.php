41f315b41eb7641663636a7c5d19b1f237803a36
ridwan <md.naeeb@gmail.com>
2020-05-21 04:52:34 +0600

    part 4

--- index-parts/part-4.php
+++ index-parts/part-4.php
@@ -1,1 +1,241 @@
-<h1>This is naeeb</h1>
+
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+ <!-- SERVICE CONTAINER-->
+
+<!--FullService Container starts here-->
+<div class="n_serviceContainer container-fluid">
+    <h2 class="wow slideInDown n_serviceHead">SERVICES</h2><br>
+    
+<!--START OF FIRST ROW-->
+    <div class="container-fluid row n_row">
+<!-- START OF CONTENT-->
+  <div class="cont col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal">
+<!--      <i style='font-size:32px' class="fas fa-pencil-alt"></i>-->
+    <span class="fas fa-pencil-alt n_serTitle" style="font-size:32px"></span><br> <b class="n_serTitle">Content</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Content</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+          <li class="list">Editing</li>
+          <li class="list">Translation</li>
+              <li class="list">Transcription</li>
+              <li class="list">Online Content</li>
+             <li class="list">Newspaper Article/Report</li>
+          <li class="list">Case Study</li>
+               <li class="list">Press Release</li>
+              <li class="list">Blog</li>
+          <li class="list">Annual Report</li>
+              <li class="list">Script</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF CONTENT-->
+        
+    <!-- START OF SOCIAL MEDIA-->
+  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal1">
+       <span class="far fa-thumbs-up n_serTitle" style="font-size:32px"></span><br> <b class="n_serTitle">Social Media</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Social Media</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+          <li class="list">Social Media Consultancy</li>
+          <li class="list">Social Profile/Content Creation</li>
+              <li class="list">Social Media Monitoring</li>
+              <li class="list">Social Media Advertising</li>
+          <li class="list">Social Media Analytics</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF SOCIAL MEDIA-->
+     
+    <!-- START OF SOCIAL WEB & APP-->
+  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal2">
+      <span class="fas fa-laptop n_serTitle" style="font-size:32px"></span><br> <b class="n_serTitle">Web & App</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Web & App</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+             <li class="list">Website Design & Development</li>
+              <li class="list">Mobile App Development</li>
+              <li class="list">Website Modification</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF WEB & APP-->
+        
+    
+            
+  </div><!-- END OF FIRST ROW -->
+
+
+<!--START OF SECOND ROW-->
+       <div class="row n_row">
+<!-- START OF DIGITAL MARKETING-->
+  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal3">
+    <span class="fas fa-chart-line n_serTitle" style="font-size:32px"></span><br><b class="n_serTitle">Digital Marketing</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel3">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Digital Marketing</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+           <li class="list">Search Engine Optimisation (SEO)</li>
+          <li class="list">Analytics Consulting</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF CONTENT-->
+        
+    <!-- START OF CREATIVE DESIGN-->
+  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal4">
+      <span class="fas fa-images n_serTitle" style="font-size:32px"></span><br> <b class="n_serTitle">Creative Design</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel4">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Creative Design</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+              <li class="list">Logo</li>
+              <li class="list">Banner</li>
+              <li class="list">Social Media Post</li>
+              <li class="list">Poster</li>
+              <li class="list">Layout</li>
+              <li class="list">Catalogue</li>
+              <li class="list">Magazine </li>
+              <li class="list">Newspaper</li>
+              <li class="list">Company Brochure</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF SOCIAL MEDIA-->
+     
+    <!-- START OF SOCIAL WEB & APP-->
+  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+  <button type="button" class="wow flipInX btn btn-primary n_serviceButton" data-toggle="modal" data-target="#myModal5">
+    <span class="fas fa-print n_serTitle" style="font-size:32px"></span><br> <b class="n_serTitle">Printing</b>
+    </button>
+  
+    <!-- Modal -->
+    <div class="modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel5">
+    <div class="modal-dialog modalBackground" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+          <p class="modal-title"><b>Printing</b></p>
+        <button type="button" class="close cross" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" class="n_cross">&times;</span></button>
+      </div>
+      <div class="modal-body">
+
+              <li class="list">Banner</li>
+              <li class="list">Poster</li>
+              <li class="list">Catalogue</li>
+              <li class="list">Magazine</li>
+              <li class="list">Brocheure</li>
+              <li class="list">Reports</li>
+              
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+      </div>
+    </div>
+    </div>
+    </div>
+    
+    </div><!-- END OF WEB & APP-->
+        
+    
+            
+  </div><!-- END OF Second ROW --> <br>
+    
+</div><!--FullService Container ends here-->
+
--- index.php
+++ index.php
@@ -8,30 +8,21 @@
     <link rel="stylesheet" href="css/dhrubo.css">
     <link rel="stylesheet" href="css/naeeb.css">
     <link rel="stylesheet" href="css/omi.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montaga" rel="stylesheet">
-    <script type="text/javascript" src="js/dhrubo.js"></script>
-    <script type="text/javascript" src="js/naeeb.js"></script>
-    <script type="text/javascript" src="js/omi.js"></script>
-    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
-    <script
-  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-  crossorigin="anonymous"></script>
-   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.5/jquery.mobile.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css'>
+    
+    <link rel="stylesheet" href="css/owl.css">
 
-   </script>
-    </script>
 
-    </script>
 
-    </script>
     <title>Omnispace</title>
   </head>
   <body>
+
+
     <?php
 
         include 'index-parts/part-1.php';
@@ -54,7 +45,20 @@
 
 
 ?>
+
+<script type="text/javascript" src="js/dhrubo.js"></script>
+<script type="text/javascript" src="js/naeeb.js"></script>
+<script type="text/javascript" src="js/omi.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+crossorigin="anonymous"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js'></script>
+<script type="text/javascript" src="js/owl.js"></script>
   </body>
+
 </html>
 
 <?php  ?>
